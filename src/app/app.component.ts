3fce9b369060cae2b61e95723b5dfaccd5120f40
Vaibhav agarwal <vaibhav@proprofs.com>
2019-09-25 17:33:25 +0530

    Observerable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myCart';
-  loadedFeature = 'recipe';
-  onNavigate(feature: string){
-  	this.loadedFeature = feature;
 
-  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
 
-  ingredientChanged = new EventEmitter<Ingredient[]>();
+  ingredientChanged = new Subject<Ingredient[]>();
 
   private ingredients : Ingredient[] = [
   new Ingredient('Apple',50),
@@ -16,7 +16,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient){
   	this.ingredients.push(ingredient);
-  	this.ingredientChanged.emit(this.ingredients.slice());
+  	this.ingredientChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]){
@@ -25,7 +25,7 @@ export class ShoppingListService {
     }
 */
     this.ingredients.push(...ingredients);
-    this.ingredientChanged.emit(this.ingredients.slice());
+    this.ingredientChanged.next(this.ingredients.slice());
   }
 
 }
